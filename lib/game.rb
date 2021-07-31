618c8f3a278d183b387133ad2e13c3e2241d8502
Matt Firmston <mattfirmston@gmail.com>
2021-07-31 12:33:18 +0100

    Game creates current frame on initialization

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,12 @@
+require_relative 'frame'
+
 class Game
+  attr_reader :current_frame
+  
+  def initialize(frame_class: Frame)
+    @current_frame = frame_class.new
+  end
+
   def roll(pins)
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,19 @@
 require 'game'
 
 describe Game do
-  it 'will except a roll' do
-    expect(subject).to respond_to(:roll).with(1).argument
+  let (:frame) { double :frame }
+  let (:frame_class) { double :frame_class, new: frame }
+
+  describe '#initialization' do  
+    it 'creates new current frame' do
+      game = Game.new(frame_class: frame_class)
+      expect(game.current_frame).to eq(frame)
+    end
+  end
+
+  describe '#roll' do
+    it 'will except a roll' do
+      expect(subject).to respond_to(:roll).with(1).argument
+    end
   end
 end
